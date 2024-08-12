d107a63903a09e9f7248255c5cb426739ead0eba
Anton Vinokurov <anton.vinokurov.qa@gmail.com>
2024-08-12 12:10:36 +0200

    add more extentions

--- convert.ps1
+++ convert.ps1
@@ -4,8 +4,11 @@ $OutputEncoding = [System.Text.Encoding]::UTF8
 $basePath = "D:\convert"
 
 # Получаем все файлы PNG и WEBP во всех вложенных папках
-$extensions = @('*.png', '*.webp')
+$extensions = @('*.png', '*.webp', '*.jpeg', '*.jpg', '*.gif')
 $files = Get-ChildItem -Path $basePath -Recurse -Include $extensions
+
+# Словарь для хранения индексов файлов в каждой папке
+$folderIndexes = @{}
 
 foreach ($file in $files) {
     # Получаем директорию текущего файла
@@ -17,12 +20,24 @@ foreach ($file in $files) {
         New-Item -ItemType Directory -Path $convertedDir
     }
 
-    # Формируем путь для сохранения конвертированного файла в формате JPG
-    $jpgFileName = [System.IO.Path]::GetFileNameWithoutExtension($file.Name) + ".jpg"
+    # Определяем имя папки
+    $folderName = [System.IO.Path]::GetFileName($directory)
+
+    # Инициализируем или получаем текущий индекс для этой папки
+    if (-not $folderIndexes.ContainsKey($folderName)) {
+        $folderIndexes[$folderName] = 1
+    }
+
+    # Формируем имя выходного файла с индексом
+    $index = $folderIndexes[$folderName]
+    $jpgFileName = "$folderName $index.jpg"
     $outputFile = Join-Path $convertedDir $jpgFileName
 
     # Применяем команду ImageMagick для конвертации в JPG
     magick "$($file.FullName)" -fuzz 10% -trim +repage -resize 1000x1000 -gravity center -background white -extent 1000x1000 "$outputFile"
+
+    # Увеличиваем индекс для следующего файла в той же папке
+    $folderIndexes[$folderName]++
 }
 
 Write-Host "Конвертация завершена."
